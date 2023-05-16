9348d1a6f2241af55451cb299b63745b13739122
Tanishq <65888542+tanishq396@users.noreply.github.com>
2023-05-16 11:58:58 +0530

    script.js

--- script.js
+++ script.js
@@ -1,75 +1,88 @@
 // Get access to video stream
 const videoElement = document.getElementById('cameraFeed');
+const overlayCanvas = document.getElementById('overlay');
+const overlayContext = overlayCanvas.getContext('2d');
 
 function startCamera() {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-      videoElement.srcObject = stream;
-    })
-    .catch(error => {
-      console.error('Error accessing camera:', error);
-    });
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+            videoElement.srcObject = stream;
+        })
+        .catch(error => {
+            console.error('Error accessing camera:', error);
+        });
 }
 
 // Function to detect spy cams
-function detectSpyCams() {
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d');
+async function detectSpyCams() {
+    const model = await cocoSsd.load();
 
-  // Set the canvas dimensions to match the video stream
-  canvas.width = videoElement.videoWidth;
-  canvas.height = videoElement.videoHeight;
+    // Detect objects in the video stream
+    const predictions = await model.detect(videoElement);
 
-  // Draw the video frame onto the canvas
-  context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+    // Check if any of the detected objects is a phone camera
+    const isPhoneCameraDetected = predictions.some(prediction => {
+        return prediction.class === 'cell phone' || prediction.class === 'mobile phone';
+    });
 
-  // Get the pixel data from the canvas
-  const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-  const pixels = imageData.data;
-
-  const spyCamColor = [255, 0, 0]; // Color values for spy cam detection (red)
-
-  // Iterate through each pixel and compare with spy cam color
-  for (let i = 0; i < pixels.length; i += 4) {
-    const red = pixels[i];
-    const green = pixels[i + 1];
-    const blue = pixels[i + 2];
-
-    // Compare the pixel color with the spy cam color
-    const colorDifference = Math.sqrt(
-      Math.pow(red - spyCamColor[0], 2) +
-      Math.pow(green - spyCamColor[1], 2) +
-      Math.pow(blue - spyCamColor[2], 2)
-    );
-
-    // If the color difference is below a certain threshold, consider it a spy cam
-    if (colorDifference < 100) {
-      return true; // Spy cam detected
-    }
-  }
-
-  return false; // No spy cams detected
+    return isPhoneCameraDetected;
 }
 
 // Start monitoring
 const startButton = document.getElementById('startButton');
 startButton.addEventListener('click', () => {
-  startCamera();
-  let isMonitoring = true;
+    startCamera();
+    let isMonitoring = true;
 
-  function monitorSpyCams() {
-    if (!isMonitoring) {
-      return;
+    async function monitorSpyCams() {
+        if (!isMonitoring) {
+            return;
+        }
+
+        const isSpyCamDetected = await detectSpyCams();
+        if (isSpyCamDetected) {
+            alert('Spy cam detected! Network security compromised.');
+            isMonitoring = false; // Stop monitoring after detection
+        } else {
+            requestAnimationFrame(monitorSpyCams); // Continue monitoring
+        }
     }
 
-    const isSpyCamDetected = detectSpyCams();
-    if (isSpyCamDetected) {
-      alert('Spy cam detected! Network security compromised.');
-      isMonitoring = false; // Stop monitoring after detection
-    } else {
-      setTimeout(monitorSpyCams, 1000); // Continue monitoring after a delay
-    }
-  }
+    monitorSpyCams();
 
-  monitorSpyCams();
+    // Draw bounding boxes for detected objects on the overlay canvas
+    function renderOverlay(predictions) {
+        overlayContext.clearRect(0, 0, overlayCanvas.width, overlayCanvas.height);
+        predictions.forEach(prediction => {
+            overlayContext.beginPath();
+            overlayContext.rect(
+                prediction.bbox[0],
+                prediction.bbox[1],
+                prediction.bbox[2],
+                prediction.bbox[3]
+            );
+            overlayContext.lineWidth = 2;
+            overlayContext.strokeStyle = 'red';
+            overlayContext.fillStyle = 'red';
+            overlayContext.stroke();
+            overlayContext.fillText(
+                `${prediction.class} (${Math.round(prediction.score * 100)}%)`,
+                prediction.bbox[0],
+                prediction.bbox[1] > 10 ? prediction.bbox[1] - 5 : 10
+            );
+        });
+    }
+
+    //Continuously update the overlay with object detection results
+    async function updateOverlay() {
+        if (!isMonitoring) {
+            return;
+        }
+        const predictions = await detectSpyCams();
+        renderOverlay(predictions);
+
+        requestAnimationFrame(updateOverlay);
+    }
+
+    updateOverlay();
 });
